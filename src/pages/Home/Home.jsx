d66ec3e96c02f94f971248f90fcc53baf641a815
AvivHitman <avivt193@gmail.com>
2022-03-16 15:04:50 +0200

    css refactor

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,37 +5,34 @@ import {
   getPersistentCandidatesData,
   transformCandidatesData,
   setPersistentCandidatesData,
+  getUpdatedCandidatesWithFavorite,
 } from "../../utils/helper.js";
 import { CardList } from "../../components/CardList/CardList";
 
 export const Home = () => {
-  const [candidates, setCandidatesFunction] = useState([]);
+  const [candidates, setCandidates] = useState([]);
 
   useEffect(() => {
     runOnHomePageLoad();
   }, []);
 
   const handleFavoriteClick = (uuid) => {
-    const updatedCandidates = Object.values(candidates).map(
-      (groupedCandidates) =>
-        groupedCandidates.map((candidate) =>
-          candidate.uuid === uuid
-            ? { ...candidate, isFavorite: !candidate.isFavorite }
-            : candidate
-        )
+    const updatedCandidates = getUpdatedCandidatesWithFavorite(
+      candidates,
+      uuid
     );
-    setCandidatesFunction(updatedCandidates);
+    setCandidates(updatedCandidates);
     setPersistentCandidatesData(updatedCandidates);
   };
 
   const runOnHomePageLoad = async () => {
     const data = getPersistentCandidatesData();
     if (data) {
-      setCandidatesFunction(data);
+      setCandidates(data);
     } else {
       const fetchedData = await fetchCandidates();
       const transformedData = transformCandidatesData(fetchedData);
-      setCandidatesFunction(transformedData);
+      setCandidates(transformedData);
       setPersistentCandidatesData(transformedData);
     }
   };
